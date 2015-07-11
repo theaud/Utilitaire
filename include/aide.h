6737d4e692ce94e4247f5edc184d22d2f4d883b7
auda <brionmathieu@gmail.com>
2015-07-11 15:37:17 +0200

    v5
    blabla

--- include/aide.h
+++ include/aide.h
@@ -3,43 +3,47 @@
 
 #include "include_utilitaire.h"
 
-/*******************************************************************************************************************/
-/*--------------------------------------------------*/
-/*        Fonction de Lecture et ecriture           */
-/*--------------------------------------------------*/
-//lecture d'une chaine de caractere dans un fichier
-char* lecture_dynamique_char(FILE * fichier);
-//lecture d'un entier dans un fichier
-int lecture_entier(FILE * fichier);
+//TOUTES LES FONCTIONS SONT  A TESTER
 
-//saisir une chaine de caractere avec espace (convertion espace et apostrophe en "_")
-char* saisie_dynamique_char();
-int saisie_entier();
+ /*****************************************************************************************/
+ /*****************      Fonction de Lecture et ecriture          *************************/
+ /*****************************************************************************************/
+                //lecture d'une chaine de caractere dans un fichier
+                char* lecture_dynamique_char(FILE * fichier);
+                //lecture d'un entier dans un fichier
+                int lecture_entier(FILE * fichier);
 
-//doner position du pointeur dans le fichier
-void position_fichier(FILE *fichier);
-/*--------------------------------------------------*/
+                //saisir une chaine de caractere avec espace (convertion espace et apostrophe en "_")
+                char* saisie_dynamique_char();
+                int saisie_entier();
 
-/*--------------------------------------------------*/
-/*       Fonction pour faciliter le codage          */
-/*--------------------------------------------------*/
+                //doner position du pointeur dans le fichier
+                void position_fichier(FILE *fichier);
 
-void saut_ligne(int i);
-int longueur_entier(int i);
-int puissance_10(int j);
-double fonctionabsolue(double nombre);
-int fonctionabsolue(int nombre);
-/*--------------------------------------------------*/
 
-/*--------------------------------------------------*/
-/*           fonction mathematique                  */
-/*--------------------------------------------------*/
-double racine_carre(double nombre);
-double cosinus(double x);
-double modulo(double x, double diviseur);
-double exponentiel(double entre);
-/*--------------------------------------------------*/
+ /*********************************************************************************/
+ /*****************       Fonction pour faciliter le codage    ********************/
+ /*********************************************************************************/
 
-char* convertir_entier_chaine(int entrer);
+        void saut_ligne(int i);
+        int longueur_entier(int i);
+
+        double fonctionabsolue(double nombre);
+        int fonctionabsolue(int nombre);
+
+ /*********************************************************************************/
+ /**************          Fonction mathematique         ***************************/
+ /*********************************************************************************/
+
+        double racine_carre(double nombre);
+        double cosinus(double x);
+        double modulo(double x, double diviseur);
+        int puissance_10(int j);
+
+ /********************************************************************************/
+ /**************************    Conversion             ***************************/
+ /********************************************************************************/
+
+        char* convertir_entier_chaine(int entrer);
 
 #endif // AIDE_H
--- src/aide.cpp
+++ src/aide.cpp
@@ -1,134 +1,15 @@
 #include "../include/aide.h"
 
-double racine_carre(double nombre)
-{
-int i;
-double valeur=nombre;
-    for(i=0;i<16;i++)
-        {if(valeur!=0)
-            {valeur=((valeur*valeur)+nombre)/(2*valeur);}
-        else
-            {valeur=0;}
-        }
-return valeur;
-}
-
-
-double cosinus(double x)
-{double somme,xcarre,valeur;
-  long i,compteur;
-
-	 x=modulo(x, 2*PI);
-	 somme = 1;
-	 xcarre = x*x;
-	 valeur = 1;
-	 i = 1;
-    compteur=0;
-	do
-	{
-    valeur=-(valeur*xcarre)/(i*(i+1));
-    somme=somme+valeur;
-    i+=2;
-    compteur++;
-	}
-	while(compteur<31);
-
-	return somme;
-}
-
-double modulo(double x, double diviseur)
-{   double sommeultat;
-sommeultat=x-diviseur*((long)(x/diviseur));
-	return sommeultat;
-}
-
-int lecture_entier(FILE * fichier)
-{int retour;
-
- fscanf(fichier,"%d",&retour);
- fseek(fichier, 2, SEEK_CUR);
-
- if(retour>20000 && ftell(fichier)==0)
-     {
-        retour=0;
-        printf("\n  votre fichier est vide , veuillez vérifier. ");
-     }
-    return retour;
-}
-
-
-char* saisie_dynamique_char()
-{
-    int i=0,t_util=0;
-    char* texte=NULL,tab[TAILLE_MAX];
-
-    for(i=0;i<TAILLE_MAX;i++){tab[i]=0;}
-
-    fgets(tab, sizeof tab, stdin);
-
-    do{t_util++;}while(tab[t_util]>0);
-    t_util--;tab[t_util]=3;
-
-    texte=(char*)malloc(sizeof(char)*(t_util+1));
-
-    for(i=0;i<t_util;i++)//transforme les espace est apostrophe en _
-        {
-            if(tab[i]==32   || tab[i]=='\'')
-            {
-             tab[i]='_';
-            }
-
-         texte[i]=tab[i];
-        }
-
-return(texte);
-}
-
-int puissance_10(int j)
-{int i;
-int resultat=1;
-    for(i=0;i<j;i++)
-    {
-        resultat=resultat*10;
-    }
-  return resultat;
-}
-
-int saisie_entier()
-{int nombre=0;
-
-    int i=0,j;
-    char tab[TAILLE_MAX];
-    char* texte=NULL;
-    int t_util=0;
-
-    for(i=0;i<TAILLE_MAX;i++){tab[i]=0;}
-
-    fgets(tab, sizeof tab, stdin);
-
-    do{t_util++;}while(tab[t_util]>0);
-    t_util--;
-    tab[t_util]=3;
-
-
-    texte=(char*)malloc(sizeof(char)*(t_util+1));
-
-    do{
-     i++;
-    }while(tab[i]>='0' && tab[i]<='9');
 
 
 
 
-    for(j=0;j<i;j++)
-    {
-    nombre+=(tab[j]-'0')*puissance_10(i-j-1);
-    }
 
-return nombre;
-}
+/*****************************************************************************************/
+ /*****************      Fonction de Lecture et ecriture          *************************/
+ /*****************************************************************************************/
 
-char* lecture_dynamique_char(FILE * fichier)
+               char* lecture_dynamique_char(FILE * fichier)
 {
     char tab[100];
     char *p=NULL;
@@ -165,24 +46,169 @@ if(test==1)
     return(p);
 }
 
+int lecture_entier(FILE * fichier)
+{int retour;
+
+ fscanf(fichier,"%d",&retour);
+ fseek(fichier, 2, SEEK_CUR);
+
+ if(retour>20000 && ftell(fichier)==0)
+     {
+        retour=0;
+        printf("\n  votre fichier est vide , veuillez vérifier. ");
+     }
+    return retour;
+}
+
+char* saisie_dynamique_char()
+{
+    int i=0,t_util=0;
+    char* texte=NULL,tab[TAILLE_MAX];
+
+    for(i=0;i<TAILLE_MAX;i++){tab[i]=0;}
+
+    fgets(tab, sizeof tab, stdin);
+
+    do{t_util++;}while(tab[t_util]>0);
+    t_util--;tab[t_util]=3;
+
+    texte=(char*)malloc(sizeof(char)*(t_util+1));
+
+    for(i=0;i<t_util;i++)//transforme les espace est apostrophe en _
+        {
+            if(tab[i]==32   || tab[i]=='\'')
+            {
+             tab[i]='_';
+            }
+
+         texte[i]=tab[i];
+        }
+
+return(texte);
+}
+
+int saisie_entier()
+{int nombre=0;
+
+    int i=0,j;
+    char tab[TAILLE_MAX];
+    char* texte=NULL;
+    int t_util=0;
+
+    for(i=0;i<TAILLE_MAX;i++){tab[i]=0;}
+
+    fgets(tab, sizeof tab, stdin);
+
+    do{t_util++;}while(tab[t_util]>0);
+    t_util--;
+    tab[t_util]=3;
+
+
+    texte=(char*)malloc(sizeof(char)*(t_util+1));
+
+    do{
+     i++;
+    }while(tab[i]>='0' && tab[i]<='9');
+
+
+
+
+    for(j=0;j<i;j++)
+    {
+    nombre+=(tab[j]-'0')*puissance_10(i-j-1);
+    }
+
+return nombre;
+}
+
+
 void position_fichier(FILE *fichier)
 {
 printf("\n le pointeur est a la position %ld.\n",ftell( fichier));
 }
 
+
+
+ /*********************************************************************************/
+ /*****************       Fonction pour faciliter le codage    ********************/
+ /*********************************************************************************/
 void saut_ligne(int nb_saut)
-{int i;
-for(i=0;i<nb_saut;i++){printf("\n");}}
+        {int i;
+        for(i=0;i<nb_saut;i++){printf("\n");}}
 
 
 int longueur_entier(int i)
-{int longueur=1;
-    while(i>9)
-    {longueur++;
-     i=i/10;
+        {int longueur=1;
+            while(i>9)
+            {longueur++;
+             i=i/10;
+            }
+          return  longueur;
+        }
+
+
+
+ /*********************************************************************************/
+ /**************          Fonction mathematique         ***************************/
+ /*********************************************************************************/
+
+double racine_carre(double nombre)
+    {
+    int i;
+    double valeur=nombre;
+        for(i=0;i<16;i++)
+            {if(valeur!=0)
+                {valeur=((valeur*valeur)+nombre)/(2*valeur);}
+            else
+                {valeur=0;}
+            }
+    return valeur;
     }
-  return  longueur;
-}
+
+double cosinus(double x)
+    {double somme,xcarre,valeur;
+      long i,compteur;
+
+         x=modulo(x, 2*PI);
+         somme = 1;
+         xcarre = x*x;
+         valeur = 1;
+         i = 1;
+        compteur=0;
+        do
+        {
+        valeur=-(valeur*xcarre)/(i*(i+1));
+        somme=somme+valeur;
+        i+=2;
+        compteur++;
+        }
+        while(compteur<31);
+
+        return somme;
+    }
+
+double modulo(double x, double diviseur)
+    {   double sommeultat;
+    sommeultat=x-diviseur*((long)(x/diviseur));
+        return sommeultat;
+    }
+
+int puissance_10(int j)
+    {int i;
+    int resultat=1;
+        for(i=0;i<j;i++)
+        {
+            resultat=resultat*10;
+        }
+      return resultat;
+    }
+
+
+  double fonctionabsolue(double nombre){}//A RECODER EN TEMPLATE
+        int fonctionabsolue(int nombre){}
+ /********************************************************************************/
+ /**************************    Conversion             ***************************/
+ /********************************************************************************/
 
 char* convertir_entier_chaine(int entrer)
 {
